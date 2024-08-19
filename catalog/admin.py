5ce3511c04a4269e49de3e026874d6a9f6104ee3
Artemteben <164046666+Artemteben@users.noreply.github.com>
2024-08-19 16:00:46 +0300

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from catalog.models import Product, Category, Blog, Version
+
+from catalog.models import Product, Category, ContactInfo, Blog, Version
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category")
+    list_display = (
+        "id",
+        "name",
+        "category",
+        "price",
+    )
     list_filter = ("category",)
-    search_fields = ("name", "description")
+    search_fields = (
+        "name",
+        "description",
+    )
 
 
 @admin.register(Category)
@@ -14,11 +23,24 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
 
 
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "phone",
+        "message",
+    )
+
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("id", "title")
+    list_display = ("title", "content", "date_creation", "publication_sign")
 
 
 @admin.register(Version)
 class VersionAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = (
+        "id",
+        "version_number",
+        "version_name",
+    )
